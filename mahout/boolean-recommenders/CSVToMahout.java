164e7c3e0f38424b1d857890bbfe8fa290ca0c5c
joseph misiti <joseph.misiti@gmail.com>
2012-10-01 23:43:14 -0400

    updated file to create kmeans input

--- mahout/boolean-recommenders/CSVToMahout.java
+++ mahout/boolean-recommenders/CSVToMahout.java
@@ -26,37 +26,34 @@ class CSVToMahout {
 
 	public static final int NUM_COLUMNS = 3;
 
+
+
 	public static void main(String[] args) throws Exception 
 	{
-
-	List<NamedVector> apples = new ArrayList<NamedVector>();
-
-	NamedVector apple;
-	
-	BufferedReader br = null;
-	br = new BufferedReader(new FileReader("ratings_300.csv"));
-	String sCurrentLine;
-	while ((sCurrentLine = br.readLine()) != null) {
 		
+		String INPUT_FILE =  "ratings_300.csv";
+		String OUTPUT_FILE = "kmeans_output_file";
 		
-		String item_name = sCurrentLine.split(",")[0];
-		double[] features = new double[NUM_COLUMNS-1];
-		for(int indx=1; indx<NUM_COLUMNS;++indx){
-			features[indx-1] = Float.parseFloat(sCurrentLine.split(",")[indx]);	
-		}
+		List<NamedVector> apples = new ArrayList<NamedVector>();
+		NamedVector apple;
+		BufferedReader br = null;
+		br = new BufferedReader(new FileReader(INPUT_FILE));
+		String sCurrentLine;
+		while ((sCurrentLine = br.readLine()) != null) {
 		
-		apple = new NamedVector(new DenseVector(features), item_name );
-		apples.add(apple);
+			String item_name = sCurrentLine.split(",")[0];
+			double[] features = new double[NUM_COLUMNS-1];
+			for(int indx=1; indx<NUM_COLUMNS;++indx){
+				features[indx-1] = Float.parseFloat(sCurrentLine.split(",")[indx]);	
+			}
+		
+			apple = new NamedVector(new DenseVector(features), item_name );
+			apples.add(apple);
 	}
-	
-
-	        
-
-	
 
 	Configuration conf = new Configuration();
 	FileSystem fs = FileSystem.get(conf);
-	Path path = new Path("appledata/apples");
+	Path path = new Path(OUTPUT_FILE);
 
 	SequenceFile.Writer writer = new SequenceFile.Writer(fs,  conf, path, Text.class, VectorWritable.class);
 
@@ -67,7 +64,7 @@ class CSVToMahout {
 	}
 	writer.close();
 
-	SequenceFile.Reader reader = new SequenceFile.Reader(fs, new Path("appledata/apples"), conf);
+	SequenceFile.Reader reader = new SequenceFile.Reader(fs, new Path(OUTPUT_FILE), conf);
 
 	Text key = new Text();
 	VectorWritable value = new VectorWritable();
